257d04de69da790774258c03ee44d9af0102ae2f
Josh <joshmfagen@gmail.com>
2021-04-13 16:06:32 -0400

    displays users and error modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AddUser from './Components/Users/AddUser';
+import UserList from './Components/Users/UserList';
 
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, { name: userName, age: userAge, id: Math.random().toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={userList}/>
     </div>
   );
 }
